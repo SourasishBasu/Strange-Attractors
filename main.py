7a1fdfa9deac77960671dadf849b017a98c6c6c1
Sasquatch <89185962+SourasishBasu@users.noreply.github.com>
2023-07-18 20:23:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 from mpl_toolkits.mplot3d import Axes3D
 
 # Dictionary containing attractor specific parameters for generating the plot
